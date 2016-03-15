3d32647422028475bee5a7bba0ab8b3f21b226df
sgopal@aquent.com <sgopal@aquent.com>
2016-03-15 13:32:40 -0500

    Search Bar set state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor (props) {
+    super(props);
+
+
+    this.state = { 'term': '' };
+  }
+
   render () {
-    return <input/>;
+    return (
+      <div>
+        <input
+          value = {this.state.term}
+          onChange = {event => this.setState({term: event.target.value})}/>
+        <p> Value of the Input: { this.state.term } </p>
+      </div>
+    );
   }
 }
-
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 
 const YOUTUBE_API_KEY = 'AIzaSyAOJ3usoBk6PjjAh2IJywTPfcdVK7huV4E';
-// index
+
+
 const App = () => {
   return <div>
-    Hi There!
+    Hi There! <br />
     <SearchBar />
   </div>;
 };
